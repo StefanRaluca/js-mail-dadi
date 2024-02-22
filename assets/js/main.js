2952f3393f7d088fc0e44ef0e077dfb5729f6d94
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-22 15:23:43 +0100

    add exercise email

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 console.log("work");
+//Faccio una const con array e dentro array scrivo i valori per i tipi di mail che sono accettati  
+const emailTypes = ["stefanraluca22@gmail", "stefanm@yahoo.it", "email1@gmail.com", "email@ymail2.com"];
+const email_user = prompt("Inserisci il tuo indirizzo email:");
+
+//Faccio una variabile con valore boolean per poter usarla nel loop (cosi da farci vedere se il valore è vero o falso)
+let emailValida = false;
+
+//Impostiamo un loop al quale diciamo che : 
+
+for (let i = 0; i < emailTypes.length; i++) {
+   //SE il utente inserisce una mail che ha un valore fra quelli indicati nel array allora Stampi in pagina come prompt  che il form è stato inviato correttamente
+    if (email_user === emailTypes[i]) {
+        emailValida = true;
+        const msg_true = prompt("Form è stato inviato correttamente!");
+       console.log(email_user);
+    }
+}
+// ALTRIMENTI stampi una alerta in pagina con il messaggio "Riprova ad digitare correttamente la tua email !"
+if (!emailValida) {
+
+    const msg_false = alert("Riprova a digitare correttamente la tua email !");
+
+    console.log(msg_false);
+}
--- readme.md
+++ readme.md
@@ -6,7 +6,8 @@ stampa un messaggio appropriato sull’esito del controllo.
 ## Steps 
 1. Faccio un input con mail e imposto che sia campo richiesto e un button submit per inviare il input
 2. Faccio una const con array e dentro array scrivo i valori per i tipi di mail che sono accettati  
-3. Impostiamo un loop al quale diciamo che : SE il utente inserisce una mail che ha un valore fra quelli indicati nel array allora Stampi in pagina come prompt o alerta che il form è stato inviato correttamente ALTRIMENTI stampi una alerta in pagina con il messaggio "Riprova ad digitare correttamente la tua email !"
+3. Faccio una variabile con valore boolean per poter usarla nel loop (cosi da farci vedere se il valore è vero o falso)
+4. Impostiamo un loop al quale diciamo che : SE il utente inserisce una mail che ha un valore fra quelli indicati nel array allora Stampi in pagina come prompt o alerta che il form è stato inviato correttamente ALTRIMENTI stampi una alerta in pagina con il messaggio "Riprova ad digitare correttamente la tua email !"
 
 ## Tools
 -const /let
